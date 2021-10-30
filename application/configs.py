79cde40fb57041a307f999ed7ab125b24109e9cc
ethilty <spinycucumber@gmail.com>
2021-10-30 04:37:11 -0500

    Config fix

--- application/configs.py
+++ application/configs.py
@@ -2,11 +2,12 @@ from .config_loader import for_mode
 
 class BaseConfig:
     MONGO_DB = 'syllabits'
+    MONGO_URI = 'mongodb://127.0.0.1:27017'
 
 @for_mode('development')
 class DevelopmentConfig(BaseConfig):
-    MONGO_URI = 'mongodb://127.0.0.1:27017'
+    DEBUG = True
 
 @for_mode('production')
 class ProductionConfig(BaseConfig):
-    pass
+    DEBUG = False
