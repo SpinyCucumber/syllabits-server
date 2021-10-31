62f1d07ecbf59a70a3856c13b40dba6b7e02ece6
ethilty <ethilty@crimson.ua.edu>
2021-10-31 02:53:41 -0500

    importcollection functional

--- application/commands.py
+++ application/commands.py
@@ -6,33 +6,36 @@ from .models import Collection, Poem
 @current_app.cli.command('import-collection')
 @click.argument('path', type=click.Path(exists=True))
 @click.argument('title')
-def import_collection(path, title):
+def importcollection(path, title):
+    click.echo(f'Importing collection \'{path}\'...')
 
     with open(path) as file:
         data = json.load(file)
 
         # Create new collection
         collection = Collection(title=title)
+        collection.save()
 
         index = 0
         prev_poem = None
-        poems = []
         for poem_data in data:
             # Create new poem and link to collection/other poems
             poem = Poem._from_son(poem_data, created=True)
             poem.collection = collection
             poem.index = index
+            poem.save()
+
             if (prev_poem):
                 poem.prev = prev_poem
                 prev_poem.next = poem
+                poem.save()
+                prev_poem.save()
+
             # Increment index
             index += 1
             prev_poem = poem
-            poems.append(poem)
         
-        # Save all poems and collection
-        for poem in poems: poem.save()
-        collection.save()
+        click.echo('Done')
         
 
 
