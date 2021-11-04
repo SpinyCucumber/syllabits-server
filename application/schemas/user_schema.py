1086e6d5cd114e6f4d8c657c471121f738b5cd0b
ethilty <ethilty@crimson.ua.edu>
2021-11-04 12:00:40 -0500

    User poem

--- application/schemas/user_schema.py
+++ application/schemas/user_schema.py
@@ -1,10 +1,11 @@
 from flask_jwt_extended import current_user
+from graphene.types.field import Field
 from graphene_mongo import MongoengineObjectType
 from graphene.relay import Node
 from graphene import (ObjectType, Schema, Mutation)
 
-from ..models import User as UserModel, Progress as ProgressModel, ProgressLine as ProgressLineModel
-from .base_schema import SubmitLine, Query as BaseQuery, Mutation as BaseMutation
+from ..models import User as UserModel, Progress as ProgressModel, Poem as PoemModel, ProgressLine as ProgressLineModel
+from .base_schema import Poem, SubmitLine, LineFeedback, Query as BaseQuery, Mutation as BaseMutation
 
 class User(MongoengineObjectType):
     class Meta:
@@ -15,10 +16,24 @@ class ProgressLine(MongoengineObjectType):
     class Meta:
         model = ProgressLineModel
         interfaces = (Node,)
+    # Whenever we send the progress, we also include feedback for the current answer
+    feedback = Field(LineFeedback)
+    # TODO
 
 class Progress(MongoengineObjectType):
     class Meta:
         model = ProgressModel
+
+# Override the base poem type to attach a progress field
+class UserPoem(Poem):
+    class Meta:
+        model = PoemModel
+        interfaces = (Node,)
+        exclude_fields = ('lines',)
+    progress = Field(Progress)
+    def resolve_progress(root, info):
+        # Look up progress using poem and user
+        return ProgressModel.objects(user=info.context['user'], poem=root).first()
 
 # This mutation overrides the base SubmitLine mutation
 # to save the user's progress before checking the actual line.
