d674301804bafab63d6e86c88a6531b7f154f67e
ethilty <ethilty@crimson.ua.edu>
2021-11-03 23:14:07 -0500

    user context

--- application/views.py
+++ application/views.py
@@ -4,8 +4,9 @@ from flask_jwt_extended import current_user
 
 from .schemas import base_schema, user_schema
 
+user_context = lambda: { 'user': current_user }
 base_view = GraphQLView.as_view("base", schema=base_schema, graphiql=app.config["ENABLE_GRAPHIQL"])
-user_view = GraphQLView.as_view("user", schema=user_schema, graphiql=app.config["ENABLE_GRAPHIQL"])
+user_view = GraphQLView.as_view("user", schema=user_schema, graphiql=app.config["ENABLE_GRAPHIQL"], get_context=user_context)
 
 @app.route('/', methods = ('GET', 'POST', 'PUT', 'DELETE'))
 def root():
