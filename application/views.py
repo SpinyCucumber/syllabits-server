3243f9701708b01a4d1c59d5b2dee0b3e4f102b6
ethilty <ethilty@crimson.ua.edu>
2021-11-03 20:57:38 -0500

    dynamic view selection

--- application/views.py
+++ application/views.py
@@ -1,13 +1,17 @@
 from flask import current_app as app
 from flask_graphql import GraphQLView
+from flask_jwt_extended import current_user
 
 from .schemas import base_schema, user_schema
 
 base_view = GraphQLView.as_view("base", schema=base_schema, graphiql=app.config["ENABLE_GRAPHIQL"])
 user_view = GraphQLView.as_view("user", schema=user_schema, graphiql=app.config["ENABLE_GRAPHIQL"])
 
-@app.route('/')
+@app.route('/', methods = ('GET', 'POST', 'PUT', 'DELETE'))
 def root():
     # Base view is default
     view = base_view
-    return base_view()
+    # If user is present, switch to user view
+    if (current_user):
+        view = user_view
+    return view()
