412e0638306fa9f3f8f274080704fd726d4219b7
ethilty <spinycucumber@gmail.com>
2021-11-19 17:57:03 -0600

    Context object

--- application/views.py
+++ application/views.py
@@ -3,13 +3,27 @@ from flask import current_app as app
 from .schemas import public_schema, user_schema
 from .flask_graphql import DynamicGraphQLView
 
+"""
+Request handling context.
+Contains information about each request, such as user.
+"""
+class Context:
+
+    user = None
+    create_refresh_token = False
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
 # Construct GraphQL view
 # We choose the schema based on the
 # authentication status of the user
 def get_schema():
     if current_user: return user_schema
     return public_schema
-
+# Use the user as the root value of the schema for convenience
 def get_root_value():
     return current_user
 
@@ -22,12 +36,12 @@ graphql = DynamicGraphQLView(
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @jwt_required(optional=True)
 def handle_request():
-    context = {'create_refresh_token': False}
+    context = Context(user=current_user)
     response = graphql.dispatch_request(context)
     # If graphql requested a refresh token to be created,
     # create a refresh token for the current user and attach it
     # as a cookie
-    if (context['create_refresh_token']):
-        token = create_refresh_token(current_user)
+    if (context.create_refresh_token):
+        token = create_refresh_token(context.user)
         set_refresh_cookies(response, token)
     return response
