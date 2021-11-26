e0e9aa537ff30bc05e05d351e4224ff8be9a809d
ethilty <spinycucumber@gmail.com>
2021-11-26 15:13:35 -0600

    Models revamp + documentation

--- application/models.py
+++ application/models.py
@@ -1,53 +1,78 @@
 from mongoengine import Document, EmbeddedDocument
-from mongoengine.fields import (EmailField, EmbeddedDocumentListField, ListField, StringField, BooleanField, ReferenceField, IntField, DateTimeField)
+from mongoengine.fields import (EmailField, EmbeddedDocumentField, EmbeddedDocumentListField, ListField, StringField, BooleanField, ReferenceField, IntField, DateTimeField)
 
-# Could create a field that wraps string field for block sequences
+"""
+Categories are like "tags" used to describe poems and collections.
+Collections are more rigid when compared to categories: they have an order and tend not to change.
+Collections are intended to be used for cataloging, assignments, etc.
+Categories are used to describe groups of similar poems. For example, a category could describe poems
+that share a common theme, were written in the same time period, etc.
+"""
+class Category(Document):
+    meta = {'collection': 'category'}
+    name = StringField()
+    references = IntField()
+
 class Collection(Document):
     meta = {'collection': 'collection'}
     title = StringField()
-
-class Tag(Document):
-    meta = {'collection': 'tag'}
-    name = StringField()
-    references = IntField()
+    """
+    Collections can have categories just like poems.
+    """
+    categories = ListField(ReferenceField(Category))
+    poems = ListField(ReferenceField('Poem'))
 
 class PoemLine(EmbeddedDocument):
     text = StringField()
     key = StringField()
     stanza_break = BooleanField(default=False)
 
+"""
+Describes a poem's relationship to a collection
+Since collections are ordered, each poem has a unique index in each collection it's a member of.
+"""
+class CollectionMember(EmbeddedDocument):
+    collection = ReferenceField(Collection)
+    index = IntField()
+
 class Poem(Document):
-    meta = {'collection': 'poem', 'indexes': ['collection']}
-    # Next, prev, index and collection are only present are in a single, ordered collection.
-    next = ReferenceField('self', required=False)
-    prev = ReferenceField('self', required=False)
-    index = IntField(required=False)
-    collection = ReferenceField(Collection, required=False)
-    tags = ListField(ReferenceField(Tag))
+    meta = {'collection': 'poem'}
+    categories = ListField(ReferenceField(Category))
     title = StringField()
     author = StringField()
     lines = EmbeddedDocumentListField(PoemLine)
+    """
+    A poem is optionally a member of a "primary collection," which is
+    effectively the "owner collection" of the poem.
+    """
+    primary_member = EmbeddedDocumentField(CollectionMember, required=False)
 
 class User(Document):
     meta = {'collection': 'user', 'indexes': ['email']}
     email = EmailField(unique=True)
     is_admin = BooleanField()
     password_hashed = StringField()
-    # A user can have multiple relationships with a poem, which can overlap, and have
-    # different rules for transitioning between them.
-    # A user can 'save' poems. This is a simple, general-purpose feature which allows
-    # users to remember poems (that they would like to play, for instance)
-    # When the user starts working on a poem, the poem is moved from saved to in-progress.
+    """
+    A user can have multiple relationships with a poem, which can overlap, and have
+    different rules for transitioning between them.
+    A user can 'save' poems. This is a simple, general-purpose feature which allows
+    users to remember poems (that they would like to play, for instance)
+    When the user starts working on a poem, the poem is moved from saved to in-progress.
+    """
     saved = ListField(ReferenceField(Poem))
-    # A list of the poems the user is currently "working on."
-    # Important to note is that all poems in-progress have an associated Progress document,
-    # but the vice-versa isn't necessarily true. A poem can be completed and not in-progress,
-    # and still have a progress document.
-    # A poem becomes in-progress by initially submitting a line of the poem.
+    """
+    A list of the poems the user is currently "working on."
+    Important to note is that all poems in-progress have an associated Progress document,
+    but the vice-versa isn't necessarily true. A poem can be completed and not in-progress,
+    and still have a progress document.
+    A poem becomes in-progress by initially submitting a line of the poem.
+    """
     in_progress = ListField(ReferenceField(Poem))
-    # A list of poems the user has completed.
-    # A poem becomes completed once all lines are correct. Once completed, a poem is no longer
-    # considered in-progress, but have become in-progress again by resetting the progress.
+    """
+    A list of poems the user has completed.
+    A poem becomes completed once all lines are correct. Once completed, a poem is no longer
+    considered in-progress, but have become in-progress again by resetting the progress.
+    """
     completed = ListField(ReferenceField(Poem))
 
 class ProgressLine(EmbeddedDocument):
@@ -61,6 +86,13 @@ class Progress(Document):
     lines = EmbeddedDocumentListField(ProgressLine)
     num_correct = IntField()
 
+"""
+Used to track invalidated tokens
+When a user logs out, their token is marked as invalidated, meaning it cannot be
+used for future login attempts. Whenever we mark the token as invalidated, we make sure
+to include an 'expireAfterSeconds' index of the expires field so that our database isn't
+polluted with old tokens.
+"""
 class TokenBlocklist(Document):
     meta = {
         'collection': 'token_blocklist',
