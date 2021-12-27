9728b2714d138209da392658d21de5438ea283cf
ethilty <spinycucumber@gmail.com>
2021-12-26 22:45:30 -0600

    import_collection -> import_poems

--- application/commands.py
+++ application/commands.py
@@ -1,39 +1,20 @@
 import click
 import json
 from flask import current_app
-from .models import Collection, Poem
+from .models import Poem
 
-@current_app.cli.command('importcollection')
+@current_app.cli.command('importpoems')
 @click.argument('path', type=click.Path(exists=True))
-@click.argument('title')
-def import_collection(path, title):
-    click.echo(f'Importing collection \'{path}\'...')
+def import_poems(path):
+    click.echo(f'Importing poems from \'{path}\'...')
 
     with open(path) as file:
         data = json.load(file)
 
-        # Create new collection
-        collection = Collection(title=title)
-        collection.save()
-
-        index = 0
-        prev_poem = None
         for poem_data in data:
-            # Create new poem and link to collection/other poems
+            # Create new poem
             poem = Poem._from_son(poem_data, created=True)
-            poem.collection = collection
-            poem.index = index
             poem.save()
-
-            if (prev_poem):
-                poem.prev = prev_poem
-                prev_poem.next = poem
-                poem.save()
-                prev_poem.save()
-
-            # Increment index
-            index += 1
-            prev_poem = poem
         
         click.echo('Done')
         
