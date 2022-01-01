f479e52d86e8b99f7d8b1b0b39e824b32a16738f
ethilty <spinycucumber@gmail.com>
2021-12-31 22:51:16 -0600

    Automatic initial ordering of poem lines

--- application/models.py
+++ application/models.py
@@ -71,11 +71,20 @@ class Poem(Document):
     title = StringField()
     author = StringField()
     lines = EmbeddedDocumentListField(PoemLine)
+
     # For testing
     # Assumes poem doesn't already belong to category
     def add_category(self, name):
         category = Category.objects(name=name).upsert_one(inc__ref_count=1)
         self.modify(push__categories=category)
+    
+    def __init__(self, **values):
+        # We automatically create an initial ordering based on order of line list
+        order = 0
+        for line in values['lines']:
+            line.order = order
+            order += 1
+        super().__init__(**values)
 
 
 class User(Document):
