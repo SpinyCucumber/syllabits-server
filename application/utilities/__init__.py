d8a9b2a96736bbee43c195148e50f47b9da06955
ethilty <spinycucumber@gmail.com>
2022-01-01 22:09:54 -0600

    Removed DynamicGraphQLView

--- application/utilities/__init__.py
+++ application/utilities/__init__.py
@@ -1,10 +1,6 @@
-from .dynamic_graphql_view import DynamicGraphQLView
 from .types import CountableConnection, CreateMutation
 
 __all__ = [
-    'DynamicGraphQLView',
-    'MapField',
-    'SearchableConnectionField',
     'CountableConnection',
     'CreateMutation'
 ]
