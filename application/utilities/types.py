0d98a82dffb9cc9ca27f34e20087dbeade78746f
ethilty <spinycucumber@gmail.com>
2022-01-05 00:47:55 -0600

    Mutate method in MongoengineCreateMutation

--- application/utilities/types.py
+++ application/utilities/types.py
@@ -22,6 +22,12 @@ class MongoengineCreateMutation(Mutation):
     def __init_subclass_with_meta__(cls, type=None, _meta=None, **options):
         assert type, 'Type is required'
         assert issubclass(type, MongoengineObjectType), 'Type must inherit from MongoengineObjectType'
+        _meta.type = type
+        # Construct mutation arguments
         arguments = {'changes': List(JSONString)}
-        # TODO
         return super().__init_subclass_with_meta__(arguments=arguments, _meta=_meta, **options)
+    
+    @classmethod
+    def mutate(cls, parent, info, changes):
+        # TODO
+        pass
