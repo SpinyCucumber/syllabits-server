f4e99a0cb3aecc429379fcd780e6509e4e5598d6
ethilty <spinycucumber@gmail.com>
2022-01-07 16:55:28 -0600

    DocumentPath documentation

--- application/utilities/document_path.py
+++ application/utilities/document_path.py
@@ -25,12 +25,22 @@ class IndexSelector:
 Selector = [FilterSelector, IndexSelector]
 
 class Level:
-    grammar = attr('property', Symbol), attr('selector', optional('[', Selector, ']'))
+    grammar = attr('field', Symbol), attr('selector', optional('[', Selector, ']'))
 
 class Path(List):
     grammar = Level, maybe_some('.', Level)
 
 class DocumentPath:
+    """
+    A "DocumentPath" is a mini-language for querying specific fields on Mongoengine documents.
+    The simplest path is a single field name. E.x: 'author'
+    Additional fields can be specified by separating fields with '.', like in Python and JS.
+    List fields can be indexed using the familiar bracket syntax. E.x. 'lines[0]'
+    Additionally, embedded document list fields can be queried using a "filter selector",
+    to select an embedded document with a certain field.
+    E.x. 'lines[id=61cfdd8dc3f63c7f1a2873b0].text'
+    A more complicated example: 'revision.lines[id=61cfdd8dc3f63c7f1a2873b0].key[2]'
+    """
 
     def __init__(self, string):
         self.path = parse(string, Path)    
@@ -38,8 +48,8 @@ class DocumentPath:
     def evaluate(self, document):
         result = document
         for level in self.path:
-            # Look up property
-            result = result[level.property.name]
+            # Look up field
+            result = result[level.field.name]
             # If selector is specified, apply selector
             if (level.selector):
                 result = level.selector.apply(result)
