93f5919de4d75aef77a6a8e27f868cc1cd8c0e7d
ethilty <ethilty@crimson.ua.edu>
2022-01-12 21:30:53 -0600

    DocumentTransform documentation

--- application/utilities/document_transform.py
+++ application/utilities/document_transform.py
@@ -68,6 +68,14 @@ def remove(receiver: list, value=None):
     receiver.remove(value)
 
 class DocumentTransform:
+    """
+    Describes a way a Mongoengine document can be modified
+    'operator' is a function that takes a receiver and arguments, and modifies
+    the receiver using the arguments
+    'path' is a DocumentPath that can be used to target a specific object inside
+    the document
+    'args' are the arguments provided to the operator
+    """
 
     def __init__(self, operator, path, args):
         self.operator = operator
