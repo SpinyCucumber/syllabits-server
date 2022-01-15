5d3a4c5663bdf1ea4500222fe0eb1826b613290c
ethilty <ethilty@crimson.ua.edu>
2022-01-15 01:24:12 -0600

    Simply typing system in document_transformer

--- application/utilities/document_transform.py
+++ application/utilities/document_transform.py
@@ -5,7 +5,7 @@ class Registry:
     def __init__(self):
         self.lookup = {}
     
-    def register(self, name, supported_types=None, required_args=None):
+    def register(self, name, supports=None, args=None):
         """
         Registers new functions as operators by decorating them
         Operators have a name, a set of supported types, and a list of required keyword args.
@@ -13,12 +13,14 @@ class Registry:
         def wrapper(func):
             def wrapped(receiver, **kwargs):
                 # Check receiver type
-                if not isinstance(receiver, supported_types):
+                if not isinstance(receiver, supports):
                     raise TypeError(f'Operator \'{name}\' does not support type \'{type(receiver).__name__}\'')
-                # Check required args
-                for arg in required_args:
+                # Check that arguments are present and match type
+                for arg, arg_type in args.items():
                     if arg not in kwargs:
                         raise TypeError(f'Missing required arg \'{arg}\'')
+                    if arg_type and not isinstance(kwargs[arg], arg_type):
+                        raise TypeError(f'Argument \'{arg}\' must be of type \'{arg_type.__name__}\'')
                 return func(receiver, **kwargs)
             self.lookup[name] = wrapped
             return wrapped
@@ -32,35 +34,35 @@ class Registry:
 
 operators = Registry()
 
-@operators.register('set', supported_types=(BaseDocument, ), required_args=('field', 'value'))
+@operators.register('set', supports=(BaseDocument, ), args={'field': str, 'value': None})
 def set(receiver: BaseDocument, field=None, value=None):
     """
     Sets a field of a document to a value
     """
     receiver[field] = value
 
-@operators.register('create', supported_types=(EmbeddedDocumentList, ), required_args=('data', ))
+@operators.register('create', supports=(EmbeddedDocumentList, ), args={'data': None})
 def create(receiver: EmbeddedDocumentList, data=None):
     """
     Inserts a new document into an embedded document list
     """
     receiver.create(**data)
 
-@operators.register('delete', supported_types=(EmbeddedDocumentList, ), required_args=('where', ))
+@operators.register('delete', supports=(EmbeddedDocumentList, ), args={'where': dict})
 def delete(receiver: EmbeddedDocumentList, where=None):
     """
     Deletes documents that meet certain conditions from an embedded document list
     """
     receiver.filter(**where).delete()
 
-@operators.register('add', supported_types=(list, ), required_args=('value', ))
+@operators.register('add', supports=(list, ), args={'value': None})
 def add(receiver: list, value=None):
     """
     Adds a value to a list
     """
     receiver.append(value)
 
-@operators.register('remove', supported_types=(list, ), required_args=('value', ))
+@operators.register('remove', supported_types=(list, ), args={'value': None})
 def remove(receiver: list, value=None):
     """
     Removes a value from a list
