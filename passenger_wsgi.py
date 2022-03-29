d1f23fc3918e29acddf34fdd51f602a8bcfbcc37
ethilty <ethilty@crimson.ua.edu>
2022-03-29 15:44:43 -0500

    SYLLABITS_PYTHON environment var

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,5 +2,13 @@
 Phusion passenger interface
 """
 
+import sys
+import os
+
+# Allow overriding python interpreter for locked-down server environments
+INTERP = os.environ.get('SYLLABITS_PYTHON')
+if INTERP and sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
 from application import create_app
 application = create_app()
